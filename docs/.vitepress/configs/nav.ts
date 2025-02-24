7d64336fe45d141bec4799fb23618770645ccf1d
whisper_xiang <xiangergou000@gmail.com>
2025-02-24 16:07:16 +0800

    docs: web3-区块链

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -4,11 +4,11 @@ export const nav: DefaultTheme.Config["nav"] = [
   { text: "前端导航", link: "/nav/" },
   {
     text: "WEB 2.0",
-    items: [{ text: "JavaScript", link: "/前端/Web2.0/JavaScript/01-简介" }],
+    items: [{ text: "JavaScript", link: "/前端/Web2/JavaScript/01-简介" }],
   },
   {
     text: "WEB 3.0",
-    items: [],
+    items: [{ text: "前言", link: "/前端/Web3/前言" }],
   },
   // { text: '茂茂主页', link: 'https://fe-mm.com' },
   // {
--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -1,18 +1,18 @@
 import type { DefaultTheme } from "vitepress";
 
 export const sidebar: DefaultTheme.Config["sidebar"] = {
-  "/前端/Web2.0/": [
+  "/前端/Web2/": [
     {
       text: "JavaScript 基础",
       collapsed: false,
-      items: [{ text: "01-简介", link: "/前端/Web2.0/JavaScript/01-简介" }],
+      items: [{ text: "01-简介", link: "/前端/Web2/JavaScript/01-简介" }],
     },
   ],
-  "/前端/Web3.0/": [
+  "/前端/Web3/": [
     {
-      text: "JavaScript 基础",
+      text: "Web3",
       collapsed: false,
-      items: [{ text: "01-简介", link: "/前端/Web2.0/JavaScript/01-简介" }],
+      items: [{ text: "前言", link: "/前端/Web3/前言" }],
     },
   ],
 };
