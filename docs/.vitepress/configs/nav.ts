40d45242c85c84a00da30bab1565685ebec63efb
轻语 <xiangergou000@gmail.com>
2025-03-05 22:54:15 +0800

    docs: devops

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -11,7 +11,8 @@ export const nav: DefaultTheme.Config["nav"] = [
       // {text: '浏览器底层原理', link: '/前端/Web2/浏览器底层原理/01-概述'},
       // { text: "ES6", link: "/前端/Web2/ES6/01-简介" },
       // { text: "TypeScript", link: "/前端/Web2/TypeScript/01-简介" },
-      { text: "前端规范", link: "/前端/Web2/前端规范/环境/环境和工具" }
+      { text: "前端规范", link: "/前端/Web2/前端规范/01-环境和工具" },
+      { text: "DevOps", link: "/前端/Web2/DevOps/01-简介" }
       // { text: "JavaScript", link: "/前端/Web2/JavaScript/01-简介" },
       // { text: "HTML", link: "/前端/Web2/HTML/01-语义化标签" },
       // { text: "CSS", link: "/前端/Web2/CSS/01-简介" },
